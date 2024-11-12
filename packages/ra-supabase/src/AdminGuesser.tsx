e985e7d5c4ed526ec338b38e105a45b88edc9a57
fzaninotto <fzaninotto@gmail.com>
2024-11-12 17:46:36 +0100

    Fix `<AdminGuesser>` type is missing the required API connection strings

--- packages/ra-supabase/src/AdminGuesser.tsx
+++ packages/ra-supabase/src/AdminGuesser.tsx
@@ -20,7 +20,7 @@ import { createClient } from '@supabase/supabase-js';
 import { defaultI18nProvider } from './defaultI18nProvider';
 
 export const AdminGuesser = (
-    props: AdminProps & { instanceUrl?: string; apiKey?: string }
+    props: AdminProps & { instanceUrl: string; apiKey: string }
 ) => {
     const {
         instanceUrl,
